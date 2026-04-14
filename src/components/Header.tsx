033c4270a1a791ea7cfe9e67adbf278f34a71a52
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-04-14 09:42:58 -0500

    Updates navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,7 @@ export default function Header() {
         itemScope
         itemType="https://schema.org/SiteNavigationElement"
       >
+        {/* LOGO */}
         <div className={styles.logo}>
           <img
             src="/logo-cobrando-online.png"
@@ -18,21 +19,59 @@ export default function Header() {
           />
         </div>
 
+        {/* MENU */}
         <ul className={styles.menu}>
+          {/* INICIO */}
           <li itemProp="name">
             <Link href="/" title="Inicio - Cobrando Online" itemProp="url">
               Inicio
             </Link>
           </li>
-          <li itemProp="name">
-            <Link
-              href="/conocenos"
-              title="Conoce a Cobrando Online y nuestro equipo"
-              itemProp="url"
-            >
-              Conocenos
-            </Link>
+
+          {/* SOLUCIONES */}
+          <li className={styles.dropdown} itemProp="name">
+            <span className={styles.dropdownTitle}>Soluciones</span>
+
+            <div className={styles.dropdownMenu}>
+              <Link href="/recuperar-cartera-vencida">
+                Recuperar cartera vencida
+              </Link>
+              <Link href="/clientes-no-pagan">Clientes que no pagan</Link>
+              <Link href="/reducir-cartera-vencida">
+                Reducir cartera morosa
+              </Link>
+              <Link href="/mejorar-flujo-de-caja">Mejorar flujo de caja</Link>
+            </div>
           </li>
+
+          {/* CÓMO FUNCIONA */}
+          <li className={styles.dropdown} itemProp="name">
+            <span className={styles.dropdownTitle}>Cómo funciona</span>
+
+            <div className={styles.dropdownMenu}>
+              <Link href="/cobro-por-whatsapp">Cobro por WhatsApp</Link>
+              <Link href="/recordatorios-de-pago">
+                Recordatorios automáticos
+              </Link>
+              <Link href="/link-de-pago">Link de pago</Link>
+              <Link href="/seguimiento-de-cartera">Seguimiento de cartera</Link>
+            </div>
+          </li>
+
+          {/* SECTORES */}
+          <li className={styles.dropdown} itemProp="name">
+            <span className={styles.dropdownTitle}>Sectores</span>
+
+            <div className={styles.dropdownMenu}>
+              <Link href="/recaudo-empresas">Empresas</Link>
+              <Link href="/recaudo-pymes">PYMES</Link>
+              <Link href="/recaudo-inmobiliarias">Inmobiliarias</Link>
+              <Link href="/recaudo-colegios">Colegios</Link>
+              <Link href="/recaudo-freelancers">Freelancers</Link>
+            </div>
+          </li>
+
+          {/* BLOG */}
           <li itemProp="name">
             <Link
               href="/blog"
@@ -42,23 +81,15 @@ export default function Header() {
               Blog
             </Link>
           </li>
-          <li itemProp="name">
-            <Link
-              href="/contactanos"
-              title="Contacta a Cobrando Online"
-              itemProp="url"
-            >
-              Contactanos
-            </Link>
-          </li>
         </ul>
 
+        {/* CTA */}
         <Link
-          href="/conocenos"
+          href="/contacto"
           className={styles.btn}
-          title="Conoce más sobre nuestros servicios de recuperación de cartera"
+          title="Empieza a recuperar tu cartera hoy"
         >
-          Conócenos
+          Empezar ahora
         </Link>
       </nav>
     </header>
